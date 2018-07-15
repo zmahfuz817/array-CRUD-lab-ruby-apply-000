154e4a045a0a79f5b0524ef6d57fe700e88ddf5c
zmahfuz817 <zunayed.lm@gmail.com>
2018-07-15 06:01:32 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ create_an_array = ['cheese','meat','goat','cow']
 end
 
 def add_element_to_end_of_array(array, element)
-create_an_array << "car"
+create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
