e5be677a6d6aeaaf2ac291d4f0fb81cb5e42b401
zmahfuz817 <zunayed.lm@gmail.com>
2018-07-15 06:05:50 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-create_an_array.pop"arrays!"
+create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
