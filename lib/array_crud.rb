74629b968344b1c6b48429516b005bbf6013bf04
zmahfuz817 <zunayed.lm@gmail.com>
2018-07-15 06:06:33 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-create_an_array = ['cheese','meat','goat','cow']
+create_an_array = ['cheese','meat','goat','arrays!']
 end
 
 def add_element_to_end_of_array(array, element)
